3491d1872aeb7c452f01e32687973d5b2346afaf
Babatunde50 <babatundeololade50@gmail.com>
2022-03-11 02:29:04 +0300

    setup apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,23 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
 import theme from './theme';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://apidev.universitesi.io/graphql',
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </ThemeProvider>,
-  document.querySelector('#root'),
+  document.querySelector('#root')
 );
